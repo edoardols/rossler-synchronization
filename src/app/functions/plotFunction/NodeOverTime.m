01128480d906f02cff0e9fb35e6743a16d274dce
VandalSnike <niccolo.petrilli@student.unisi.it>
2023-12-17 22:54:02 +0100

    Changed positions of buttons

--- src/app/functions/plotFunction/NodeOverTime.m
+++ src/app/functions/plotFunction/NodeOverTime.m
@@ -1,26 +1,28 @@
 function NodeOverTime(t,trj)
     disp('Hello world')
-    mainFig = figure('Name', 'Node over time', 'NumberTitle', 'off', 'Position', [100, 100, 800, 400]);
+    mainFig = figure('Name', 'Node over time', 'NumberTitle', 'off', 'Position', [100, 100, 800, 600]);
     sgtitle('Node over time','Color','r');
     subplot1 = subplot(1, 3, 1);
     subplot2 = subplot(1, 3, 2);
     subplot3 = subplot(1, 3, 3);
 
    
-    spinner = uicontrol('Style', 'edit', 'Position', [20, 10, 50, 30]);
+    spinner = uicontrol('Style', 'edit', 'Position', [360, 10, 50, 30]);
 
-    confirmButton = uicontrol('Style', 'pushbutton', 'String', 'Conferma', 'Position', [150, 10, 100, 30], 'Callback', @confirm);
+    confirmButton = uicontrol('Style', 'pushbutton', 'String', 'Confirm', 'Position', [420, 10, 50, 30], 'Callback', @confirm);
 
    
     currentNumber =0;
 
     function confirm(~, ~)
         currentNumber = str2double(get(spinner, 'String'));
-        if currentNumber < 1 || currentNumber > size(trj, 2)/3
-         error('N error!');
+        if currentNumber < 1 || currentNumber > size(trj, 2)/3 || isempty(currentNumber)
+         set(spinner, 'ForegroundColor','red','FontWeight', 'bold');     
+        else
+         disp(currentNumber)
+         set(spinner, 'ForegroundColor', [0.4660 0.6740 0.1880],'FontWeight','normal'); 
+         plotSubplots(currentNumber);
         end
-        disp(currentNumber)
-        plotSubplots(currentNumber);
     end
 
     function plotSubplots(number)
