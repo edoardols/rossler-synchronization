f88a8c823a94fdedd11e2a261f3941a7a696f4d0
VandalSnike <niccolo.petrilli@student.unisi.it>
2023-12-17 22:55:55 +0100

    Test for testing SaveGUI script

---
+++ src/test/TestSaveGUI.m
@@ -0,0 +1,13 @@
+N=2;
+Adj=[0 1;1 0];
+IC = [3 5;7 10];
+F=[1 1];
+A=[8 9];
+B=[2 1];
+C=[1 7];
+ % text_to_display = sprintf('N = %s\nAdj = %s\nCI = %s\nF = %s\nA = %s\nB = %s\nC = %s\n', ...
+ %    mat2str(N), mat2str(Adj), mat2str(IC), mat2str(F), mat2str(A), mat2str(B), mat2str(C));
+ % 
+SaveGUI(N, Adj, IC, F, A, B, C);
+
+   
